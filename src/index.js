13597efe2b799f1f40def411df36da109df701c0
Naira Kazeian <nkazeian@gmail.com>
2021-08-08 22:04:52 +0300

    realized themeChange

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './sass/main.scss';
+import menu from "./menu.json";  
+import './styles.css';
+import renderTemplate from './template.hbs';
+
+const menuMarkup = createMenuMarkup(menu);
+function createMenuMarkup(menu) {
+  // return menu.map(menuItem=>renderTemplate(menuItem)).join('');
+  return menu.map(renderTemplate).join('');
+}
+
+
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// }; ---- ИСПОЛЬЗОВАТЬ
+
+
+
+const toolbarColor = document.querySelector('body');
+const inputChange = document.querySelector('#theme-switch-toggle');
+inputChange.addEventListener('click', colorChange);
+
+function colorChange() {
+  const switcher = toolbarColor.classList.toggle('dark-theme');
+  localStorage.setItem('switch', switcher);
+};
+

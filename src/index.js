99f8339ef9c952b94a376b6850e50dcdf8093101
Naira Kazeian <nkazeian@gmail.com>
2021-08-09 08:33:38 +0300

    rendered menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import menu from "./menu.json";  
 import './styles.css';
-import renderTemplate from './template.hbs';
+import menuTemplate from './template.hbs';
 
 const menuMarkup = createMenuMarkup(menu);
 function createMenuMarkup(menu) {
-  // return menu.map(menuItem=>renderTemplate(menuItem)).join('');
-  return menu.map(renderTemplate).join('');
-}
+  // return menu.map(menuItem=>menuTemplate(menuItem)).join('');
+  return menu.map(menuTemplate).join('');
+};
 
+const menuRef = document.querySelector('.js-menu');
+console.log(menuRef);
+menuRef.insertAdjacentHTML('beforeend', menuMarkup);
 
 // const Theme = {
 //   LIGHT: 'light-theme',
